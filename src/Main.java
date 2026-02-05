8e907741adf0a783bcba03af7916174e868781c8
Dmitry Malakhov <newmerzostb@gmail.com>
2026-02-05 14:20:05 +0300

    fix: task2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 2;
-        System.out.println("Сумма: " + (a + b));
-        System.out.println("Разность: " + (a - b));
-        System.out.println("Произведение: " + (a * b));
-        System.out.println("Деление:" + (a / b));
-        System.out.println("Остаток от деления: " + (a % b));
+        int x = 7;
+        //int y = x++;
+        int y = ++x;
+        System.out.println(y);
 
     }
 }
