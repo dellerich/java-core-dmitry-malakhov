122e8dc182b69d79fc924d7aeba9a6f367dccb34
Dmitry Malakhov <newmerzostb@gmail.com>
2026-02-05 15:09:18 +0300

    fix: task5

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 20;
-        boolean hasTicket = true;
-        if (age >= 18 && hasTicket) {
-            System.out.println("Проход разрешён");
-        } else {
-            System.out.println("Проход запрещён");
-        }
+       int number = 10;
+       if (number > 0){
+           System.out.println("Положительное");
+       } else if (number == 0){
+           System.out.println("Ноль");
+
+       } else {
+           System.out.println("Отрицательное");
+       }
     }
 }
