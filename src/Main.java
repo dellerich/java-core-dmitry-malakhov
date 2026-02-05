fe444542fffdf66b8d5fd9f7571e4289a34d586e
Dmitry Malakhov <newmerzostb@gmail.com>
2026-02-05 15:02:12 +0300

    fix: task3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 7;
-        //int y = x++;
-        int y = ++x;
-        System.out.println(y);
+        int a = 3;
+        int b = 9;
+        if  (a > b) {
+            System.out.println(true);
+        } else if (a == b){
+            System.out.println(true);
+        } else if (a != b) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
 
     }
 }
