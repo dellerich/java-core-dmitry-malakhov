18dd38ac57b08471a35c1eb7c06a2f5d8e247f6b
Dmitry Malakhov <newmerzostb@gmail.com>
2026-02-05 13:48:17 +0300

    feat: task1 and add gitignore

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println("Сумма: " + (a + b));
+        System.out.println("Разность: " + (a - b));
+        System.out.println("Произведение: " + (a * b));
+        System.out.println("Деление:" + (a / b));
+        System.out.println("Остаток от деления: " + (a % b));
+
+    }
+}
