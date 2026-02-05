a747025284a7b9f1b2db35e87120430d7c97bbfc
Dmitry Malakhov <newmerzostb@gmail.com>
2026-02-05 15:23:23 +0300

    fix: task7

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-       int x = 30;
-       int y = 20;
+       int day = 3;
+       switch (day) {
+           case 1:
+               System.out.println("Понедельник");
+               break;
+           case 2:
+               System.out.println("Вторник");
+               break;
+           case 3:
+               System.out.println("Среда");
+               break;
+           case 4:
+               System.out.println("Четверг");
+               break;
+           case 5:
+               System.out.println("Пятница");
+               break;
+           case 6:
+               System.out.println("Суббота");
+               break;
+           case 7:
+               System.out.println("Воскресенье");
+               break;
+           default:
+               System.out.println("Неккоректный день");
+       }
 
-       int max = x > y ? x : y;
-       System.out.println(max);
     }
 }
